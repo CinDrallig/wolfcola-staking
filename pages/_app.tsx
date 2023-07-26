ae0a338f2d7acf0a9a2635bf15d3369854cfaf9c
Cin Drallig <104178930+CinDrallig@users.noreply.github.com>
2023-07-26 01:24:09 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,25 @@ import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
+import { Pulsechain } from "@thirdweb-dev/chains";
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+
+// If used on the FRONTEND pass your 'clientId'
+const sdk = new ThirdwebSDK(Pulsechain, {
+  clientId: "fb5b78b67cf8fd462d6adb08086cc98b",
+});
+// --- OR ---
+// If used on the BACKEND pass your 'secretKey'
+const sdk = new ThirdwebSDK(Pulsechain, {
+  secretKey: "mCicoRCbEq0C_ZaoQKLiHiA9YSsp-kClhMQveKnSoJbVZvC6jvCunNWhNAnQXYae0KtrQqnMeHSIKPxF5mi36Q",
+});
+
+const contract = await sdk.getContract("0x1D4F18d768755C19E036055fC5E597AC6740088a");
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "pulsechain";
+const activeChain = "Pulsechain";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
