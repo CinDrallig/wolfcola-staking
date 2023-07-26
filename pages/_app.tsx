ceb0bd0af71f53e2af05e933b1f7515a8cdc64fb
Cin Drallig <104178930+CinDrallig@users.noreply.github.com>
2023-07-26 01:47:05 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,10 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
 import { Pulsechain } from "@thirdweb-dev/chains";
 
+const sdk = new ThirdwebSDK("Pulsechain", {
+  clientId: "fb5b78b67cf8fd462d6adb08086cc98b"
+});
+
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
@@ -12,8 +16,7 @@ const activeChain = "Pulsechain";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
    <ThirdwebProvider
-      activeChain={Pulsechain}
-      clientId="fb5b78b67cf8fd462d6adb08086cc98b">
+      activeChain={Pulsechain}>
       <ChakraProvider>
         <Navbar />
         <Component {...pageProps} />
